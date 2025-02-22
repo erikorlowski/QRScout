788b484769e1b4e72f6730c4e9b3328444a00bc9
erikorlowski <erik.orlowski@gmail.com>
2025-02-22 01:14:22 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/QRScout/',
   plugins: [react()],
   resolve: {
     alias: {
